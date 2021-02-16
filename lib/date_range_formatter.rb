550c549259aaa1ee785e9e57979556246a718304
Gabba <gabriellamds@yahoo.it>
2021-02-16 19:35:32 +0100

    Extract methods that format date ranges

--- lib/date_range_formatter.rb
+++ lib/date_range_formatter.rb
@@ -18,25 +18,9 @@ class DateRangeFormatter
     full_end_date = format_full_date(end_date)
 
     if start_date == end_date
-      if start_time && end_time
-        "#{full_start_date} at #{start_time} to #{end_time}"
-      elsif start_time
-        "#{full_start_date} at #{start_time}"
-      elsif end_time
-        "#{full_start_date} until #{end_time}"
-      else
-        full_start_date
-      end
+      format_same_date(full_start_date)
     else
-      if start_time && end_time
-        "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
-      elsif start_time
-        "#{full_start_date} at #{start_time} - #{full_end_date}"
-      elsif end_time
-        "#{full_start_date} - #{full_end_date} at #{end_time}"
-      else
-        "#{full_start_date} - #{full_end_date}"
-      end
+      format_different_dates(full_start_date, full_end_date)
     end
   end
 
@@ -44,5 +28,29 @@ class DateRangeFormatter
 
   def format_full_date(date)
     date.strftime("#{DayOrdinalizer.ordinalize(date.day)} %B %Y")
+  end
+
+  def format_same_date(full_start_date)
+    if start_time && end_time
+      "#{full_start_date} at #{start_time} to #{end_time}"
+    elsif start_time
+      "#{full_start_date} at #{start_time}"
+    elsif end_time
+      "#{full_start_date} until #{end_time}"
+    else
+      full_start_date
+    end
+  end
+
+  def format_different_dates(full_start_date, full_end_date)
+    if start_time && end_time
+      "#{full_start_date} at #{start_time} - #{full_end_date} at #{end_time}"
+    elsif start_time
+      "#{full_start_date} at #{start_time} - #{full_end_date}"
+    elsif end_time
+      "#{full_start_date} - #{full_end_date} at #{end_time}"
+    else
+      "#{full_start_date} - #{full_end_date}"
+    end
   end
 end
